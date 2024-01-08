84c9c411751f6604f437ae1722427fffbb6368fb
Ashlesha1012 <ashleshachoukade@gmail.com>
2024-01-08 15:28:38 +0530

    all

--- MyInsurance/settings.py
+++ MyInsurance/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'MyInsurance.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_insurance',       # Replace with your database name
+        'USER': 'root',            # Replace with your MySQL username
+        'PASSWORD': 'root',              # Replace with your MySQL password
+        'HOST': 'localhost',               # Replace with your MySQL host (if not localhost)
+        'PORT': '3306',                    # Replace with your MySQL port (if not default)
     }
 }
+
 
 
 # Password validation
